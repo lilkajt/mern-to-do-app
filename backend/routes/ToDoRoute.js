b16a8e5b8de653f7df589a04a80d05c3795c86ea
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-02 02:04:21 +0100

    complete routes

--- backend/routes/ToDoRoute.js
+++ backend/routes/ToDoRoute.js
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
+import { createToDo, deleteToDo, getToDos, updateToDo } from '../controller/ToDo.controller';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    // res.send("Hello World");
-    res.json({message: "Hello World"});
-});
+router.get('/', getToDos);
+router.get('/', createToDo);
+router.get('/:id', updateToDo);
+router.get('/:id', deleteToDo);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import ToDoRoutes from './routes/ToDoRoute.js';
 
 const app = express();
 app.use(express.json());
-app.use('/api/todo', ToDoRoutes);
+app.use('/api/todos', ToDoRoutes);
 app.listen(7500, ()=>{
     connectDB();
     console.log("Server started at http://localhost:7500");
