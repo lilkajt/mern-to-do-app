9521b172ac11842c9598063854c66ed6bd2dd0c9
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-03 21:54:41 +0100

    Navbar component test

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="h-14 w-full flex justify-center items-center bg-gray-800 p-1.5 text-slate-100 font-bold">
+      <h1 className='hover:bg-sky-700 rounded px-1 cursor-pointer'>
+          To Do
+      </h1>
+    </div>
+  )
+}
+
+export default Navbar
