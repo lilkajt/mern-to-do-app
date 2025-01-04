37584485ac5baa771113d746e00060abfced5f8b
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-04 20:06:50 +0100

    theme toggle

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { FaRegMoon } from 'react-icons/fa';
+import { FiSun } from "react-icons/fi";
 
-function Navbar() {
-  return (
-    <div className="h-14 w-full flex justify-center items-center bg-gray-800 p-1.5 text-slate-100 font-bold">
-      <h1 className='hover:bg-sky-700 rounded px-1 cursor-pointer'>
-          To Do
-      </h1>
-    </div>
-  )
+type NavbarProps = {
+  onAddTask: () => void;
+  toggleTheme: () => void;
+  theme: string;
 }
 
-export default Navbar
+// To Do - bigger(https://dribbble.com/shots/15574940-Fitness-app - right page), hover - text on blue
+// maybe add later animation 'to' drops and 'do' moves to left and next to it drops 'NOW!' and text glows brighter
+const Navbar: React.FC<NavbarProps> = ({ onAddTask, toggleTheme, theme }) => {
+  return (
+    <div className="h-24 w-full flex justify-center items-center dark:bg-gray-800 bg-gray-400 p-1.5 text-slate-100 font-bold select-none">
+      <div className="title text-4xl">
+        <h1 className='hover:text-green-300 px-1 cursor-pointer'>
+          To Do
+        </h1>
+      </div>
+      <div className="space w-24"></div>
+      <div className="addTask border-2 rounded-lg border-green-500 bg-green-500 hover:bg-green-700 px-2 py-1">
+        <button type="button" onClick={onAddTask}>Add Task +</button>
+      </div>
+      <div className="space w-24"></div>
+      <div className="darkMode">
+        <button type="button" className='' onClick={toggleTheme}>
+          {theme === "light" ? <FaRegMoon /> : <FiSun />}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
