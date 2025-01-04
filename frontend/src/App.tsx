78bb2d50fcfcfa2da18a12e41b4faac4af403f78
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-04 20:07:59 +0100

    Routing + toggle theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
-function App() {
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const App: React.FC = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === 'dark') {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <>
-      <Navbar />
+      <Navbar onAddTask={() => {}} toggleTheme={toggleTheme} theme={theme} />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
