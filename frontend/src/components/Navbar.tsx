0215d3c9edf697119ccc9ef764b1fe764928aaba
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-05 20:50:29 +0100

    Navbar styles completed

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,25 +8,24 @@ type NavbarProps = {
   theme: string;
 }
 
-// To Do - bigger(https://dribbble.com/shots/15574940-Fitness-app - right page), hover - text on blue
 // maybe add later animation 'to' drops and 'do' moves to left and next to it drops 'NOW!' and text glows brighter
 const Navbar: React.FC<NavbarProps> = ({ onAddTask, toggleTheme, theme }) => {
   return (
-    <div className="h-24 w-full flex justify-center items-center dark:bg-gray-800 bg-gray-400 p-1.5 text-slate-100 font-bold select-none">
-      <div className="title text-4xl">
-        <h1 className='hover:text-green-300 px-1 cursor-pointer'>
+    <div className="main h-24 w-full flex items-center justify-between dark:bg-gray-700 bg-gray-400 p-1.5 text-slate-100 font-bold select-none">
+      <div className="title text-4xl flex-grow text-center">
+        <h1 className='inline-block p-2 hover:text-sky-500 cursor-pointer'>
           To Do
         </h1>
       </div>
-      <div className="space w-24"></div>
+      <div className="flex items-center space-x-4">
       <div className="addTask border-2 rounded-lg border-green-500 bg-green-500 hover:bg-green-700 px-2 py-1">
-        <button type="button" onClick={onAddTask}>Add Task +</button>
+        <button type="button" onClick={onAddTask}>Task +</button>
       </div>
-      <div className="space w-24"></div>
-      <div className="darkMode">
-        <button type="button" className='' onClick={toggleTheme}>
-          {theme === "light" ? <FaRegMoon /> : <FiSun />}
+      <div className="toggle">
+        <button type="button" className='w-12 h-12 flex items-center justify-center' onClick={toggleTheme}>
+          {theme === "light" ? <FaRegMoon size={24} /> : <FiSun size={24} />}
         </button>
+      </div>
       </div>
     </div>
   );
