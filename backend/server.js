10e14263b7e18cb6b2d7a51bfa869bf0f3ec8256
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-16 19:35:14 +0100

    production build

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ import express from 'express';
 import {connectDB} from './config/db.js';
 import ToDoRoutes from './routes/ToDoRoute.js';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
+const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json());
 app.use('/api/todos', ToDoRoutes);
+if ( process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+}
 app.listen(7500, ()=>{
     connectDB();
     console.log("Server started at http://localhost:7500");
